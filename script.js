07be980ab2cc66598030f459868fcaff31b7911f
Varrium <rolandogarciaram@gmail.com>
2022-08-03 21:06:14 -0700

    Add js file skeleton

---
+++ script.js
@@ -0,0 +1,13 @@
+let myLibrary = [];
+
+function Book(title, author, pages, status) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.status = status
+}
+
+function addBookToLibrary() {
+    
+    const book = Object.create(Book)
+}
